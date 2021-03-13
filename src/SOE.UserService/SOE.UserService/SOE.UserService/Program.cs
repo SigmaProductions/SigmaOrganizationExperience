c754784309dde6882c053e96024866ddf04879e8
MacenDi <mstanekofficial@gmail.com>
2021-03-13 23:01:06 +0100

    SOE rename

--- src/SOE.UserService/SOE.UserService/SOE.UserService/Program.cs
+++ src/SOE.UserService/SOE.UserService/SOE.UserService/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SigmaOrganizationExperience.UserService
+namespace SOE.UserService
 {
     public class Program
     {
